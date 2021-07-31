3cee5015ac7d3ab70ed3656732d235d058140c85
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-07-30 19:57:08 -0700

    try1

--- sum-all-odd-fibonacci-numbers.js
+++ sum-all-odd-fibonacci-numbers.js
@@ -1,14 +1,15 @@
 module.exports = function (num) {
   // write your code here
-function sumFibs(num) {
+ function sumFibs(num) {
   let m = 0
   let n = 1
   let count = 0
   while (n <=num) {
     [n, m] = [n+m, n]
     if (n%2 !==0){
-      count+= n;
+      count+= n
     }
   }
+  return count;
 }
 }
